bd95a6093b24476e1860e5776d5c123c2975111a
Almaz <almaz.kzn2005@yandex.ru>
2025-05-08 18:23:07 +0300

    feat: - add OAuth2ClientConfig - add RabbitConfig

---
+++ backend/google-meeting-service/src/main/java/ru/calendorny/googlemeetingservice/config/RabbitConfig.java
@@ -0,0 +1,70 @@
+package ru.calendorny.googlemeetingservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.calendorny.googlemeetingservice.properties.RabbitProperties;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitConfig {
+
+    private final RabbitProperties properties;
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue googleMeetQueue() {
+        return QueueBuilder.durable(properties.googleMeetQueue()).build();
+    }
+
+    @Bean
+    public Queue meetingLinksQueue() {
+        return QueueBuilder.durable(properties.meetingLinksQueue()).build();
+    }
+
+    @Bean
+    public DirectExchange meetingCreateExchange() {
+        return ExchangeBuilder.directExchange(properties.meetingCreateExchange())
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public DirectExchange meetingLinksExchange() {
+        return ExchangeBuilder.directExchange(properties.meetingLinksExchange())
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Binding googleMeetBinding(Queue googleMeetQueue, DirectExchange meetingCreateExchange) {
+        return BindingBuilder.bind(googleMeetQueue).to(meetingCreateExchange).with(properties.googleMeetRoutingKey());
+    }
+
+    @Bean
+    public Binding meetingLinkBinding(Queue meetingLinksQueue, DirectExchange meetingLinksExchange) {
+        return BindingBuilder.bind(meetingLinksQueue).to(meetingLinksExchange).with(properties.meetingLinkRoutingKey());
+    }
+}
