f938398077ea2bcc29c7e88df96994c2177462f3
shcherbanev <shcherbanev1@yandex.ru>
2025-05-18 15:52:43 +0300

    feat: add files for app running

--- backend/auth-service/src/main/java/ru/calendorny/authservice/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/ru/calendorny/authservice/AuthServiceApplication.java
@@ -2,12 +2,14 @@ package ru.calendorny.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.calendorny.authservice.config.JwtProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
-
 }
