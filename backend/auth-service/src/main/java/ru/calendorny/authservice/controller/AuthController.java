e0651b5503dbb4291c28f967bfcf87bdf7f18072
shcherbanev <shcherbanev1@yandex.ru>
2025-05-20 14:33:08 +0300

    feat: add exception handling for controllers

--- backend/auth-service/src/main/java/ru/calendorny/authservice/controller/AuthController.java
+++ backend/auth-service/src/main/java/ru/calendorny/authservice/controller/AuthController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.calendorny.authservice.config.JwtProperties;
 import ru.calendorny.authservice.dto.request.LoginRequest;
 import ru.calendorny.authservice.dto.request.RegisterRequest;
+import ru.calendorny.authservice.dto.response.ApiErrorResponse;
 import ru.calendorny.authservice.dto.response.AuthTokens;
+import ru.calendorny.authservice.exception.LoginException;
+import ru.calendorny.authservice.exception.RegistrationException;
 import ru.calendorny.authservice.service.AuthService;
 
 @RestController
@@ -28,9 +32,7 @@ public class AuthController {
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public Map<String, String> register(@RequestBody @Valid RegisterRequest request) {
-        return Map.of(
-                "userId",
-                authService.register(request.email(), request.password()).toString());
+        return Map.of("userId", authService.register(request).toString());
     }
 
     @PostMapping("/login")
@@ -39,16 +41,10 @@ public class AuthController {
         AuthTokens tokens = authService.login(request.email(), request.password());
 
         String cookieValue = "refreshToken=%s; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=%d"
-                .formatted(tokens.refreshToken(), jwtProperties.refreshTokenExpirationDays() * 24L * 60L * 60L);
+            .formatted(tokens.refreshToken(), jwtProperties.refreshTokenExpirationDays() * 24L * 60L * 60L);
         response.addHeader("Set-Cookie", cookieValue);
 
         return new AuthTokens(tokens.accessToken(), null);
-    }
-
-    @PostMapping("/refresh")
-    @ResponseStatus(HttpStatus.OK)
-    public AuthTokens refresh(@CookieValue("refreshToken") String refreshToken) {
-        return authService.refresh(refreshToken);
     }
 
     @PostMapping("/logout")
@@ -58,4 +54,26 @@ public class AuthController {
         String expiredCookie = "refreshToken=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0";
         response.addHeader("Set-Cookie", expiredCookie);
     }
+
+    @PostMapping("/refresh")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthTokens refresh(@CookieValue("refreshToken") String refreshToken) {
+        AuthTokens authTokens = authService.refresh(refreshToken);
+        return new AuthTokens(authTokens.accessToken(), null);
+    }
+
+    @ExceptionHandler(RegistrationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrorResponse handleRegistrationException(RegistrationException ex) {
+        ApiErrorResponse resp = new ApiErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getClass().getSimpleName(), ex.getMessage());
+        return resp;
+    }
+
+    @ExceptionHandler(LoginException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ApiErrorResponse handleLoginException(LoginException ex) {
+        ApiErrorResponse resp = new ApiErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getClass().getSimpleName(), ex.getMessage());
+        return resp;
+    }
+
 }
--- backend/auth-service/src/main/java/ru/calendorny/authservice/controller/ProfileController.java
+++ backend/auth-service/src/main/java/ru/calendorny/authservice/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package ru.calendorny.authservice.controller;
 
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,11 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import ru.calendorny.authservice.dto.response.UserProfile;
 import ru.calendorny.authservice.security.AuthenticatedUser;
+import ru.calendorny.authservice.service.ProfileService;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class ProfileController {
+
+    private final ProfileService profileService;
 
     @GetMapping("/public")
     @ResponseStatus(HttpStatus.OK)
@@ -23,9 +29,7 @@ public class ProfileController {
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/profile")
     @ResponseStatus(HttpStatus.OK)
-    public Map<String, String> getProfile(@AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
-        return Map.of(
-                "id", authenticatedUser.id().toString(),
-                "email", authenticatedUser.email());
+    public UserProfile profile(@AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
+        return profileService.getUserProfile(authenticatedUser.id());
     }
 }
