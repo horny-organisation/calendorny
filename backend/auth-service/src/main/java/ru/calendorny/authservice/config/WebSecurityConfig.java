02c13f7d4210848a4c1553ca9b9d2aeafa4a61e0
shcherbanev <shcherbanev1@yandex.ru>
2025-05-21 00:21:43 +0300

    fix: fix security config

--- backend/auth-service/src/main/java/ru/calendorny/authservice/config/WebSecurityConfig.java
+++ backend/auth-service/src/main/java/ru/calendorny/authservice/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.calendorny.authservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.calendorny.authservice.dto.response.ApiErrorResponse;
 import ru.calendorny.authservice.security.JwtAuthenticationFilter;
 
 @Configuration
@@ -42,7 +44,17 @@ public class WebSecurityConfig {
                 .permitAll())
             .exceptionHandling(eh -> eh
                 .authenticationEntryPoint((request, response, authException) -> {
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setContentType("application/json");
+
+                    ApiErrorResponse error = new ApiErrorResponse(
+                        401,
+                        authException.getClass().getSimpleName(),
+                        authException.getMessage()
+                    );
+
+                    ObjectMapper mapper = new ObjectMapper();
+                    response.getWriter().write(mapper.writeValueAsString(error));
                 })
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
