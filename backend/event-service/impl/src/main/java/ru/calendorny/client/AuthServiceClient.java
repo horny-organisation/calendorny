825e73d4d18b6a197f9585e322da3b6af3ddebc9
VeronikaYamancheva <vshrony@mail.ru>
2025-06-14 11:23:45 +0300

    feature: add simple feign client

--- backend/event-service/impl/src/main/java/ru/calendorny/client/AuthServiceClient.java
+++
@@ -1,7 +0,0 @@
-package ru.calendorny.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "auth-service")
-public class AuthServiceClient {
-}
---
+++ backend/event-service/impl/src/main/java/ru/calendorny/util/feignclient/AuthServiceClient.java
@@ -0,0 +1,19 @@
+package ru.calendorny.util.feignclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.UUID;
+
+@FeignClient(name = "auth-service")
+public interface AuthServiceClient {
+
+    //TODO: точные endpoint-ы у auth-service
+
+    @GetMapping("/api/v1/auth/users")
+    UUID getUserIdByEmail(@RequestParam("email") String email);
+
+    @GetMapping("/api/v1/auth/users/{userId}")
+    String getEmailByUserId(@PathVariable("userId") UUID userId);
+}
